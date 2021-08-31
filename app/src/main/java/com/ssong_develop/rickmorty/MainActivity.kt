b1ae44c20c4be846db3e0f7bc9651bdc2bb7575e
SSong-develop <thdgnsrl123@naver.com>
2021-08-31 15:36:39 +0900

    auto Cleared Value

--- app/src/main/java/com/ssong_develop/rickmorty/MainActivity.kt
+++ app/src/main/java/com/ssong_develop/rickmorty/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.ssong_develop.rickmorty
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.ssong_develop.rickmorty.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
