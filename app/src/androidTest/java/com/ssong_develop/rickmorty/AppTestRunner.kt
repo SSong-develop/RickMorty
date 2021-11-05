df74a8954e53c18ebae2f1df2c49ed0670b170a3
SSong-develop <thdgnsrl123@naver.com>
2021-11-05 15:14:08 +0900

    AppTestRunner

---
+++ app/src/androidTest/java/com/ssong_develop/rickmorty/AppTestRunner.kt
@@ -0,0 +1,15 @@
+package com.ssong_develop.rickmorty
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+@Suppress("unused")
+class AppTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(
+        cl: ClassLoader?,
+        className: String?,
+        context: Context?
+    ): Application = super.newApplication(cl, HiltTestApplication::class.java.name, context)
+}
