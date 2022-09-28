919bfe105d9b605f5074f0737b7e3d4df8f3bc86
SSong-develop <thdgnsrl123@naver.com>
2022-09-29 00:45:43 +0900

    생각

--- core-data/src/main/java/com/ssong_develop/core_data/network/pagingsource/CharacterPagingSource.kt
+++ core-data/src/main/java/com/ssong_develop/core_data/network/pagingsource/CharacterPagingSource.kt
@@ -7,6 +7,7 @@ import com.ssong_develop.core_model.Characters
 import kotlinx.coroutines.delay
 import javax.inject.Inject
 
+// TODO( api가 디자인을 url로 디자인을 해서 url로 페이징을 하는게 좋아보입니다. )
 class CharacterPagingSource @Inject constructor(
     private val characterService: CharacterService
 ) : PagingSource<Int, Characters>() {
@@ -25,8 +26,6 @@ class CharacterPagingSource @Inject constructor(
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Characters> {
         val currentKey = params.key ?: STARTING_KEY
 
-        if (currentKey != STARTING_KEY) delay(500L)
-
         val response = runCatching {
             characterService.getCharacters(currentKey)
         }.getOrElse { throwable ->
@@ -34,6 +33,9 @@ class CharacterPagingSource @Inject constructor(
         }
 
         val prevKey = if (currentKey == STARTING_KEY) null else currentKey - 1
+
+        // next로 다음에 볼 수 있는 url을 넘겨줍니다.
+        // url중 page 변수만 찾아다가 하면 될 거 같은데요??
 
         return runCatching {
             LoadResult.Page(
