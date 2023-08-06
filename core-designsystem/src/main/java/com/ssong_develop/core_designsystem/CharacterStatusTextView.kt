8b972ad556932baf2e3555fd15cae1da00fbf0a7
SSong-develop <thdgnsrl123@naver.com>
2023-08-06 23:50:37 +0900

    characterStatusTextView description

--- core-designsystem/src/main/java/com/ssong_develop/core_designsystem/CharacterStatusTextView.kt
+++ core-designsystem/src/main/java/com/ssong_develop/core_designsystem/CharacterStatusTextView.kt
@@ -1,11 +1,13 @@
 package com.ssong_develop.core_designsystem
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.ContextCompat
 
+/**
+ * 캐릭터의 상태에 따라(죽었는지, 살았는지, 모르는지) 표시 해주는 텍스트 뷰
+ */
 class CharacterStatusTextView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
