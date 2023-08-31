9445d1b4a58608590cd140800d6b7cd3d4d11d56
SSong-develop <thdgnsrl123@naver.com>
2023-08-31 23:51:37 +0900

    upgrade episode api call

---
+++ core-common/src/main/java/com/ssong_develop/core_common/listener/OnDebounceClickListener.kt
@@ -0,0 +1,19 @@
+package com.ssong_develop.core_common.listener
+
+import android.view.View
+
+typealias OnClickListener = (View) -> Unit
+
+class OnDebounceClickListener(private val listener: OnClickListener): View.OnClickListener {
+    override fun onClick(view: View?) {
+        val now = System.currentTimeMillis()
+        if (now < lastTime + INTERVAL) return
+        lastTime = now
+        view?.run(listener)
+    }
+
+    companion object {
+        private const val INTERVAL: Long = 200L
+        private var lastTime: Long = 0
+    }
+}
--- core-data/src/main/java/com/ssong_develop/core_data/datasource/CharacterDataSource.kt
+++ core-data/src/main/java/com/ssong_develop/core_data/datasource/CharacterDataSource.kt
@@ -1,11 +1,15 @@
 package com.ssong_develop.core_data.datasource
 
+import com.ssong_develop.core_common.RateLimiter
 import com.ssong_develop.core_data.service.CharacterService
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class CharacterDataSource @Inject constructor(
     private val service: CharacterService
 ) {
+    private val dataSourceRateLimiter = RateLimiter<String>(timeout = 1, timeUnit = TimeUnit.SECONDS)
+
     suspend fun getCharacterEpisode(urls: List<String>) =
-        urls.map { url -> service.getEpisodes(url) }
+        urls.takeIf { dataSourceRateLimiter.shouldFetch(urls.first()) }?.map { url -> service.getEpisodes(url) }
 }
